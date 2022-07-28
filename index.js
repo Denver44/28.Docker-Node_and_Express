f853640db5fb56c883d46761a3c9d988f4b1a9bf
Denver <70090559+Denver44@users.noreply.github.com>
2022-07-28 14:35:03 +0530

    Basic App ready

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res)=>{
-  res.send('<h1>Helle Docker</h1>')
+app.get('/', (req, res) => {
+  res.send('<h1>Helle Docker</h1>');
 });
 
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
-})
+});
 
 export default app;
