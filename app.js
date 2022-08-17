b24193795ceb27c75e257039790de155c1bb293f
Denver <70090559+Denver44@users.noreply.github.com>
2022-08-18 03:10:59 +0530

    Half Done

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 import express from 'express';
-const app = express();
+import session from 'express-session';
+import redis from 'redis';
+import connectRedis from 'connect-redis';
 import postRouter from './routes/postRoutes.js';
 import userRouter from './routes/userRoutes.js';
 
+const { REDIS_IP, REDIS_PORT, SESSION_SECRET } = process.env;
+
+const app = express();
+const RedisStore = connectRedis(session);
+
+const redisClient = redis.createClient({
+  port: REDIS_PORT,
+  host: REDIS_IP,
+});
+
 app.use(express.json());
+
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      secure: false, // if true: only transmit cookie over https
+      httpOnly: true, // if true: prevents client side JS from reading the cookie
+      maxAge: 30000, // session max age in milliseconds
+      sameSite: 'lax', // make sure sameSite is not none
+    },
+  })
+);
 
 app.use('/post', postRouter);
 app.use('/user', userRouter);
--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,7 @@ const signUp = async (req, res) => {
       password: hashedPassword,
     });
 
-    // req.session.user = newUser;
+    req.session.user = newUser;
 
     res.status(201).json({
       status: 'success',
@@ -41,7 +41,7 @@ const login = async (req, res) => {
     const isCorrect = await bcrypt.compare(password, user.password);
 
     if (isCorrect) {
-      // req.session.user = user;
+      req.session.user = user;
       res.status(200).json({
         status: 'success',
       });
